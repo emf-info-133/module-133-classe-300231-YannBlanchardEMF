3dbb81ee2650fec8c54621a7a28dd539ece696ee
JoelRibeiroDaRocha <joel.ribeiro@studentfr.ch>
2025-04-07 14:39:59 +0200

    ENTREPRISE

--- projet/serviceRestENTREPRISE/code/src/main/java/main/code/controller/Controller.java
+++ projet/serviceRestENTREPRISE/code/src/main/java/main/code/controller/Controller.java
@@ -6,14 +6,11 @@ import main.code.service.MenuService;
 import main.code.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class Controller {
+public class Controller implements ItfController {
+
     private final UserService userService;
     private final MenuService menuService;
 
@@ -23,39 +20,33 @@ public class Controller {
         this.userService = userService;
     }
 
-    // Handler pour GET
-    @GetMapping("/")
+    @Override
     public String getNothing() {
         return "";
     }
 
-    @PostMapping(path = "/addMenu")
-    public @ResponseBody String addNewMenu(@RequestParam String nom, @RequestParam Integer prix_unitaire,
-            @RequestParam Integer userId) {
+    @Override
+    public String addNewMenu(String nom, Integer prix_unitaire, Integer userId) {
         return menuService.addNewMenu(nom, prix_unitaire, userId);
     }
 
-    @PostMapping(path = "/modifyMenu")
-    public @ResponseBody String modifyMenu(@RequestParam Integer pk_menu, @RequestParam String nom,
-            @RequestParam Integer prix_unitaire, @RequestParam Integer userId) {
+    @Override
+    public String modifyMenu(Integer pk_menu, String nom, Integer prix_unitaire, Integer userId) {
         return menuService.modifyMenu(pk_menu, nom, prix_unitaire, userId);
     }
 
-    @PostMapping(path = "/deleteMenu")
-    public @ResponseBody String deleteMenu(@RequestParam Integer pk_menu, @RequestParam Integer userId) {
+    @Override
+    public String deleteMenu(Integer pk_menu, Integer userId) {
         return menuService.deleteMenu(pk_menu, userId);
     }
 
-    @GetMapping(path = "/getMenu")
-    public @ResponseBody Iterable<MenuDTO> getAllUsers() {
-        // This returns a JSON or XML with the users
+    @Override
+    public Iterable<MenuDTO> getAllUsers() {
         return menuService.findAllMenu();
     }
 
-    @GetMapping(path = "/getUser")
-    public @ResponseBody Iterable<UserResponse> getAllUser() {
-        // This returns a JSON or XML with the users
+    @Override
+    public Iterable<UserResponse> getAllUser() {
         return userService.findAllUser();
     }
-
 }
--- projet/serviceRestENTREPRISE/code/src/main/java/main/code/controller/ItfController.java
+++ projet/serviceRestENTREPRISE/code/src/main/java/main/code/controller/ItfController.java
@@ -1,5 +1,35 @@
 package main.code.controller;
 
-public class ItfController {
+import main.code.dto.MenuDTO;
+import main.code.dto.UserResponse;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+public interface ItfController {
+
+    @GetMapping("/")
+    String getNothing();
+
+    @PostMapping(path = "/addMenu")
+    @ResponseBody
+    String addNewMenu(@RequestParam String nom, @RequestParam Integer prix_unitaire,
+                      @RequestParam Integer userId);
+
+    @PostMapping(path = "/modifyMenu")
+    @ResponseBody
+    String modifyMenu(@RequestParam Integer pk_menu, @RequestParam String nom,
+                      @RequestParam Integer prix_unitaire, @RequestParam Integer userId);
+
+    @PostMapping(path = "/deleteMenu")
+    @ResponseBody
+    String deleteMenu(@RequestParam Integer pk_menu, @RequestParam Integer userId);
+
+    @GetMapping(path = "/getMenu")
+    @ResponseBody
+    Iterable<MenuDTO> getAllUsers();
+
+    @GetMapping(path = "/getUser")
+    @ResponseBody
+    Iterable<UserResponse> getAllUser();
 }
